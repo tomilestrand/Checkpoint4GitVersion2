a9f001369684274a435ad58fdc707f46d7d98c06
Tom Ilestrand <tomilestrand@gmail.com>
2018-11-04 21:41:23 +0100

    first commit

---
+++ Checkpoint4ver2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Checkpoint4ver2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
