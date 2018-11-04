f59a5aebcc9792aba70a3bd3bd1fb0f2b274e36c
Tom Ilestrand <tomilestrand@gmail.com>
2018-11-04 21:50:48 +0100

    added movie 5

--- Checkpoint4ver2/Program.cs
+++ Checkpoint4ver2/Program.cs
@@ -8,6 +8,7 @@ namespace Checkpoint4ver2
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Best movies ever!");
+            Console.WriteLine("Infinite Prejudice");
         }
     }
 }
