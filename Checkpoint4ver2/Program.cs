f85a4bfe6115771c4a9dd928080241b320f33b7d
Tom Ilestrand <tomilestrand@gmail.com>
2018-11-04 21:42:35 +0100

    added label

--- Checkpoint4ver2/Program.cs
+++ Checkpoint4ver2/Program.cs
@@ -7,6 +7,7 @@ namespace Checkpoint4ver2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Best movies ever!");
         }
     }
 }
